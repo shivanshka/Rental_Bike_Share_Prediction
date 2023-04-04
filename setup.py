a49aea766e362fa697d44efe7bed1fa43da7c791
Shivansh Kaushal <96131265+shivanshka@users.noreply.github.com>
2023-04-04 15:53:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
 #    install_requires=get_requirements_list()
 )
 
-if __name__=="__main__":
-    print(get_requirements_list())
+#if __name__=="__main__":
+#    print(get_requirements_list())
 
 
